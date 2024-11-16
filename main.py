036195de8e97c40add09c37098d8a002c0d80c9b
nktbashl <nb9374131@gmail.com>
2024-11-16 18:10:38 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+
+print (9 ** 0.5 * 5 )  # st1 program
+print (9.99 > 9.98 and 1000 != 1000.1) #2nd program
+print (2 * 2 + 2 ) # 3nd program
+print (2 * (2 + 2) )
+print ( (2 * 2 + 2 ) == (2 * (2 + 2) ))
+print(int(float('123.456') * 10) % 10) # 4th program
